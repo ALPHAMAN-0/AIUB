8a5a17ced203717d2921ca9e1826c87cf8e21501
Siam <huffmancoding.acm@gmail.com>
2025-05-20 06:59:01 +0600

    no message

--- DS Lab/DS Lab Task (Convert Dobbly to Single List).cpp
+++ DS Lab/DS Lab Task (Convert Dobbly to Single List).cpp
@@ -1,88 +1,97 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-struct Node {
+struct Node
+{
     int data;
-    Node* next;
-    Node* prev;
-    
-    Node(int Value) {
+    Node *next;
+    Node *prev;
+
+    Node(int Value)
+    {
         data = Value;
         next = NULL;
         prev = NULL;
     }
 };
 
-struct Node* head = NULL;
-struct Node* tail = NULL;
+struct Node *head = NULL;
+struct Node *tail = NULL;
 
-void InsertIntoLast(int Value) {
-    Node* newNode = new Node(Value);
-    if (head == NULL) {
+void InsertIntoLast(int Value)
+{
+    Node *newNode = new Node(Value);
+    if (head == NULL)
+    {
         head = newNode;
         tail = newNode;
-    } else {
+    }
+    else
+    {
         tail->next = newNode;
         newNode->prev = tail;
         tail = newNode;
     }
 }
 
-
-void Display(){
-    Node* current = head;
-    while(current != NULL){
+void Display()
+{
+    Node *current = head;
+    while (current != NULL)
+    {
         cout << current->data << " ";
         current = current->next;
     }
     cout << endl;
 }
-void ReverseDisplay() {
-    Node* current = tail;
-    if(tail == NULL) {
+void ReverseDisplay()
+{
+    Node *current = tail;
+    if (tail == NULL)
+    {
         cout << "List is empty, nothing to display." << endl;
         return;
     }
-    while (current != NULL) {
+    while (current != NULL)
+    {
         cout << current->data << " ";
         current = current->prev;
     }
     cout << endl;
 }
-void ConvertToSingleList() {
-    Node* current = head;
-    while (current != NULL) {
-        Node* nextNode = current->next;
+void ConvertToSingleList()
+{
+    Node *current = head;
+    while (current != NULL)
+    {
+        Node *nextNode = current->next;
         current->prev = NULL; // Remove the previous link
-        current->next = NULL; // Remove the next link
         current = nextNode;
     }
     tail = NULL; // Clear the tail as well
-}   
-int main(){
-    //Insert value into last of the dobbly Linked List
+}
+int main()
+{
+    // Insert value into last of the dobbly Linked List
     InsertIntoLast(10);
     InsertIntoLast(20);
     InsertIntoLast(30);
     InsertIntoLast(40);
-    
-    cout<< "Display of Dobbly Linked List: ";
+
+    cout << "Display of Dobbly Linked List: ";
     Display();
-    cout<<endl;
-    cout<< "Reverse Display of Dobbly Linked List: ";
+    cout << endl;
+    cout << "Reverse Display of Dobbly Linked List: ";
     ReverseDisplay();
-    cout<<endl;
-    
+    cout << endl;
+
     ConvertToSingleList();
-    cout<< "Display of Single Linked List after conversion: ";
+    cout << "Display of Single Linked List after conversion: ";
     Display();
-    cout<<endl;
-    cout<< "Reverse Display of Single Linked List after conversion: ";
+    cout << endl;
+    cout << "Reverse Display of Single Linked List after conversion: ";
     ReverseDisplay();
-    cout<<endl;
-    
-
-
+    cout << endl;
 
     return 0;
 }
